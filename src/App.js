1735662df89b56655da2a64c2be4a6043b800e0d
Korwen3 <47819671+Korwen3@users.noreply.github.com>
2020-10-22 12:36:37 +0200

    get done
    get done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Services } from './http-services';
 
 export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allPosts: []
+        }
+    }
 
     render() {
-        return <div>it worked !</div>
+        return <>
+            <ul>
+                {this.state.allPosts.map((post) => <li>{post.id}, {post.content}, {post.comment}</li>)}
+            </ul>
+        </>
+    }
+
+    componentDidMount() {
+        Services.getAllPosts().then((results) => {
+            this.setState({
+                allPosts: results.data
+            });
+            console.log(this.allPosts);
+        });
     }
 }
